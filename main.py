dd71fae1c1aadd9f9d49111be6bf1ff571c7acf4
Naimche <ncheelk111@g.educaand.es>
2024-05-06 18:48:05 +0200

    v1.2

--- main.py
+++ main.py
@@ -21,41 +21,33 @@ def calcular_hash(file_path, hash_type):
 
 
 def verificar_archivos(csv_path, folder_path, output_file=None):
-    # Leer el archivo CSV
     df = pd.read_csv(csv_path, header=None, names=['Archivo', 'MD5', 'SHA256', 'Ruta'])
 
-    # Crear una lista para almacenar los resultados
     resultados = []
 
-    # Iterar sobre cada fila del DataFrame
     for index, row in df.iterrows():
         ruta_archivo = os.path.join(folder_path, row['Ruta'])
 
         # Verificar si el archivo existe
         if os.path.exists(ruta_archivo):
-            # Calcular los hashes del archivo actual
             md5_original = calcular_hash(ruta_archivo, 'md5')
             sha256_original = calcular_hash(ruta_archivo, 'sha256')
 
-            # Comparar los hashes
             verificado = (row['MD5'] == md5_original) and (row['SHA256'] == sha256_original)
 
-            # Agregar resultados a la lista
-            resultados.append([row['Archivo'], row['MD5'], md5_original, row['SHA256'], sha256_original, verificado])
+            nombre_archivo = os.path.basename(row['Ruta'])
+
+            resultados.append([nombre_archivo, row['MD5'], md5_original, row['SHA256'], sha256_original, verificado])
         else:
-            # Si el archivo no se encuentra, agregamos un mensaje de error
             resultados.append(
                 [row['Archivo'], row['MD5'], 'Archivo no encontrado', row['SHA256'], 'Archivo no encontrado', False])
 
-    # Crear un DataFrame a partir de los resultados
     df_resultados = pd.DataFrame(resultados, columns=['Archivo', 'MD5 (hash proporcionado)', 'MD5 (hash original)',
                                                       'SHA256 (hash proporcionado)', 'SHA256 (hash original)',
                                                       'Verificado'])
 
-    # Generar la tabla en formato Markdown
     markdown_table = generar_tabla_markdown(df_resultados)
 
-    # Si se proporciona un archivo de salida, escribir la tabla en ese archivo
     if output_file:
         with open(output_file, 'w') as f:
             f.write(markdown_table)
@@ -65,7 +57,6 @@ def verificar_archivos(csv_path, folder_path, output_file=None):
 
 
 def generar_tabla_markdown(df):
-    # Crear encabezados de columna
     headers = "|"
     separator = "|"
     for col in df.columns:
@@ -74,7 +65,6 @@ def generar_tabla_markdown(df):
     headers += "\n"
     separator += "\n"
 
-    # Crear filas
     rows = ""
     for _, row in df.iterrows():
         row_str = "|"
@@ -83,7 +73,6 @@ def generar_tabla_markdown(df):
         row_str += "\n"
         rows += row_str
 
-    # Combinar todo
     markdown_table = headers + separator + rows
     return markdown_table
 
